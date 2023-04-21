04659f05eb3e255bd808347557576b4dc82a75d6
Ramiro Garces <ramgarces24@gmail.com>
2023-04-21 10:20:12 -0500

    Add styling to nav bar

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,20 +1,56 @@
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/teamSelect">Team Selection</RouterLink>
-    <RouterLink to="/pokemonStats">Pokemon Stats</RouterLink>
-    <RouterLink to="/teams">Teams</RouterLink>
-    <RouterLink to="/user">User</RouterLink>
-    <RouterLink to="/login">Login</RouterLink>
-  </nav>
+<nav>
+    <div class="wrapper">
+        <div class="menu">
+            <RouterLink to="/" class="menu-item">Home</RouterLink>
+            <RouterLink to="/teamSelect" class="menu-item">
+                Team Selection
+            </RouterLink>
+            <RouterLink to="/pokemonStats" class="menu-item">
+                Pokemon Stats
+            </RouterLink>
+            <RouterLink to="/teams" class="menu-item">Teams</RouterLink>
+        </div>
+        <div class="login">
+            <RouterLink to="/user" class="login-name">User</RouterLink>
+            <RouterLink to="/login" class="login-login">Login</RouterLink>
+        </div>
+    </div>
+</nav>
 </template>
 
 <style lang="postcss" scoped>
 nav {
-  @apply flex justify-center space-x-4 p-4;
+    @apply flex h-20 bg-red-700 text-yellow-300 px-6;
 
-  & .router-link-active {
-    @apply underline underline-offset-4;
-  }
+    & .router-link-active {
+        @apply underline underline-offset-4;
+    }
+
+    .wrapper {
+        @apply container mx-auto flex w-full items-center justify-between;
+
+        .menu {
+            @apply flex gap-4;
+
+            &-item {
+                @apply rounded-md px-4 py-2 hover:bg-yellow-500 
+                hover:text-blue-800;
+            }
+        }
+
+        .login {
+            @apply flex gap-10;
+
+            &-name {
+                @apply rounded-md px-4 py-2 hover:bg-yellow-500 
+                hover:text-blue-800;
+            }
+            &-login {
+                @apply rounded-md px-4 py-2 hover:bg-yellow-500 
+                hover:text-blue-800;
+            }
+        }
+    }
 }
 </style>
