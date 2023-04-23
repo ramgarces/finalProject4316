cc2a88e0581cb15415fc2edc88fe9f33dda0771d
Ramiro Garces <ramgarces24@gmail.com>
2023-04-23 13:41:24 -0500

    Add cards to Teams and User pages

--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -1,5 +1,35 @@
+<script setup>
+import SixStack from '@/components/SixStack.vue';
+</script>
+
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">User Page</h1>
-  </main>
+  <p class="teams-title">User's Teams</p>
+  <div class="wrapper">
+    <p class="teams-1">Your Team #1</p>
+    <SixStack />
+    <button type="button">View Details</button>
+    <p class="teams-2">Your Team #2</p>
+    <SixStack />
+    <button type="button">View Details</button>
+    <p class="teams-3">Your Team #3</p>
+    <SixStack />
+    <button type="button">View Details</button>
+  </div>
 </template>
+
+<style lang="postcss" scoped>
+  .wrapper {
+    @apply container flex flex-col mx-auto py-6;
+    p {
+      @apply p-6 font-semibold text-5xl text-yellow-400;
+    }
+    button {
+      @apply text-center p-4 font-semibold text-2xl text-yellow-400 
+      bg-red-600 w-60 self-center mt-6 rounded-md ;
+    }
+  }
+  .teams-title {
+    @apply text-center p-6 font-semibold text-5xl text-yellow-400
+      tracking-wide;
+  }
+</style>
