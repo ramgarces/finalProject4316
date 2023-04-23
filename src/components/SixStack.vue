beafd7402c3d4e6a1c2179636cd50d220de011bf
Ramiro Garces <ramgarces24@gmail.com>
2023-04-23 13:21:30 -0500

    Add six stack and cards to Team Selection

---
+++ src/components/SixStack.vue
@@ -0,0 +1,16 @@
+<script setup>
+    import SingleCard from './SingleCard.vue'
+</script>
+
+
+<template>
+    <div class="sub-wrapper">
+        <SingleCard v-for="n in 6"/>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+    .sub-wrapper {
+        @apply grid grid-cols-6 gap-4;
+    }
+</style>
--- src/views/TeamSelectPage.vue
+++ src/views/TeamSelectPage.vue
@@ -1,10 +1,18 @@
 <script setup>
   import SearchBar from '@/components/SearchBar.vue';
   import MainCards from '@/components/MainCards.vue';
+  import SixStack from '@/components/SixStack.vue';
 </script>
 
 <template>
   <div class="wrapper">
+    <p class="instructions-six-stack">
+      Click a Pokémon to remove it from your team
+    </p>
+    <div class="six-stack-background">
+      <SixStack class="six-stack"/>
+      <button type="button">Save Team</button>
+    </div>
     <SearchBar />
     <p class="instructions">Select a Pokémon to add them to your team</p>
     <MainCards />
@@ -14,5 +22,17 @@
 <style lang="postcss" scoped>
   .wrapper {
     @apply container flex flex-col mx-auto py-6;
+    p {
+      @apply text-center pb-6 font-semibold text-2xl text-yellow-400
+      tracking-widest;
+    }
+    button {
+      @apply text-center p-6 font-semibold text-2xl text-yellow-400
+      tracking-widest bg-red-600 w-60 self-center mt-6 rounded-md;
+    }
+    .six-stack-background {
+      @apply flex flex-col sticky top-0 z-50 bg-blue-700 w-full p-10
+      rounded-md;
+    }
   }
 </style>
