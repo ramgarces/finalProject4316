d27c2fae22b388f337bd050c3e5cafb135763842
Ramiro Garces <ramgarces24@gmail.com>
2023-04-23 13:54:20 -0500

    Add Pokeball image to Home page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,21 @@
 <script setup>
-import { ref } from 'vue'
-const title = ref('Home Page 🎈')
+  import { ref } from 'vue'
+  const title = ref('Home Page 🎈')
 </script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
-  </main>
+  <div class="wrapper">
+    <div class="card-image">
+      <img src="../assets/Poké_Ball_icon.svg.png" alt="" srcset="" />
+    </div>
+  </div>
 </template>
+
+<style lang="postcss" scoped>
+  .wrapper {
+    @apply container flex flex-col mx-auto py-6;
+    .card-image {
+      @apply mx-auto;
+    }
+  }
+</style>
